d4326fd9d2fcb8e56da8f7af09513790ebb614e5
Jon Hieb <hiebj@live.com>
2015-02-10 11:02:24 -0700

    use require-all instead of manual requires

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function GulpDir() {
   var fs = require('fs'),
     path = require('path'),
     gulp = require('gulp'),
+    requireAll = require('require-all'),
     rootDir = process.env.INIT_CWD,
     dir = path.join(rootDir, 'gulp'),
     args = arguments,
@@ -13,20 +14,20 @@ function GulpDir() {
     dir = path.join(rootDir, 'gulp.d')
   }
 
-  fs.readdirSync(dir).forEach(loadGulpModule);
-  function loadGulpModule(module) {
-    var fullpath;
-    if (module.match(/^.*\.js$/)) {
-      fullpath = path.join(dir, module);
-      module = require(fullpath);
-      if (module) {
-        if (typeof module === 'function') {
-          module = module.apply(module, args);
-        }
-        for (var metaTask in module) {
-          metaTasks[metaTask] = metaTasks[metaTask] || [];
-          metaTasks[metaTask].push(module[metaTask]);
-        }
+  require('require-all')({
+    dirname: dir,
+    filter: /^.*\.js$/,
+    resolve: resolve
+  });
+
+  function resolve(module) {
+    if (module) {
+      if (typeof module === 'function') {
+        module = module.apply(module, args);
+      }
+      for (var metaTask in module) {
+        metaTasks[metaTask] = metaTasks[metaTask] || [];
+        metaTasks[metaTask].push(module[metaTask]);
       }
     }
   }
